c04682c6da8068924d8c9418de6a543f1aa9f26c
may-leth <mayleth9609@gmail.com>
2025-09-17 14:45:40 +0200

    add .env to gitignore

---
+++ src/main/java/com/hackaton/recuerdamed/RecuerdamedApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.recuerdamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuerdamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuerdamedApplication.class, args);
+	}
+
+}
