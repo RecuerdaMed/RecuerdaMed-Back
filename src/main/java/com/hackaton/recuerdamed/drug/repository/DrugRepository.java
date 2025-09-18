ab1f06f2640508f04747571924361940cc5b714c
may-leth <mayleth9609@gmail.com>
2025-09-18 17:19:34 +0200

    test: add integration test to updateDrug method and delete comment in cors configuration

--- src/main/java/com/hackaton/recuerdamed/drug/repository/DrugRepository.java
+++ src/main/java/com/hackaton/recuerdamed/drug/repository/DrugRepository.java
@@ -3,6 +3,7 @@ package com.hackaton.recuerdamed.drug.repository;
 import com.hackaton.recuerdamed.drug.entity.Drug;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalTime;
@@ -15,6 +16,6 @@ public interface DrugRepository extends JpaRepository<Drug, Long> {
     Optional<Drug> findByIdAndActiveTrue(Long id);
     List<Drug> findByDrugNameContainingIgnoreCaseAndActiveTrue(String drugName);
 
-    @Query("SELECT m FROM Drug m WHERE m.active = true AND m.reminderActive = true AND m.nextIntake <= :currentTime")
-    List<Drug> findDrugsForReminder(LocalTime currentTime);
+    @Query("SELECT d FROM Drug d WHERE d.active = true AND d.activeReminder = true AND d.nextIntakeTime <= :currentTime")
+    List<Drug> findDrugsForReminder(@Param("currentTime") LocalTime currentTime);
 }
