8536fd32bfba3b1da939fbf77735af63e8de3664
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-18 10:38:20 +0200

    fix: change post to put in updateDrug method of DrugController

--- src/main/java/com/hackaton/recuerdamed/drug/controller/DrugController.java
+++ src/main/java/com/hackaton/recuerdamed/drug/controller/DrugController.java
@@ -35,7 +35,7 @@ public class DrugController {
         return ResponseEntity.status(HttpStatus.CREATED).body(drug);
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<DrugResponse> updateDrug(@PathVariable Long id, @Valid @RequestBody DrugRequest request) {
         DrugResponse drug = drugService.updateDrug(id, request);
         return ResponseEntity.status(HttpStatus.OK).body(drug);
